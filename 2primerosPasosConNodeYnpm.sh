fdcfc8cf68aa5d3f821483e0816257d48ebde829
repositorioinformatico <jd.garciaperez@edu.gva.es>
2022-12-09 10:24:25 +0000

    Arreglando script 2

--- 2primerosPasosConNodeYnpm.sh
+++ 2primerosPasosConNodeYnpm.sh
@@ -1,5 +1,16 @@
-mkdir node01
-cd node01
-npm init
-npm install --save-dev moment -y
-npm install --save-dev chalk -y
+#mkdir node01
+#cd node01
+#npm init
+#npm install --save-dev moment -y
+#npm install --save-dev chalk -y
+cat<<EOF > main.js
+//Prueba del paquete chalk 
+import chalk from 'chalk';
+console.log(chalk.blue('Puesta en')); 
+console.log(chalk.red('Produccion')); 
+console.log(chalk.green('Segura')); 
+//Prueba del paquete moment
+import moment from 'moment';
+var fecha = moment('2022-11-23'); 
+console.log(fecha.format()); 
+EOF
